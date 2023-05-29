fb6a914530a35005afb63d67c682e03670ba4287
DinHammer <dinhammer@gmail.com>
2023-05-29 15:45:50 +0300

    initComit

---
+++ XamarinEvernote/XamarinEvernote/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XamarinEvernote.ViewModels;
+using XamarinEvernote.Views;
+
+namespace XamarinEvernote
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ XamarinEvernote/XamarinEvernote/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinEvernote.Models;
+using XamarinEvernote.ViewModels;
+
+namespace XamarinEvernote.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
